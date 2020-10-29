228f55b61f6fbca8413b2f9861cb61a9b0817433
Rene Triebenstein <rene.triebenstein@me.com>
2020-10-29 11:20:16 +0100

    Create if-Abfrage.py

---
+++ if-Abfrage.py
@@ -0,0 +1,14 @@
+# IF-Abfragen Grundlagen
+
+# Variablen deklarieren
+zahl = 5
+
+# IF-Abfrage
+if zahl == 5:
+    print("Zahl ist gleich 5")
+if zahl == 4:
+    print("Zahl ist gleich 4")
+if zahl <= 5:
+    print("Zahl ist kleiner oder gleich 5")
+
+print("Ende")
